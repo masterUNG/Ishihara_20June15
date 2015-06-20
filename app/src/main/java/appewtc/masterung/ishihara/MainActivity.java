644b10a393fa588841ba5727cc7bd4c62f7f41f8
masterUNG <phrombutr@gmail.com>
2015-06-20 15:09:35 +0700

    Day1

--- app/src/main/java/appewtc/masterung/ishihara/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ishihara/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    }   // onCreate
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +34,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class
